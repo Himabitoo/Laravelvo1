2fe74d893448fc8b1a4211c6a99128db500abe9b
NoXeL <luxeno103@gmail.com>
2022-02-21 18:28:04 +0900

    create login/logout

--- resources/views/login/login_form.blade.php
+++ resources/views/login/login_form.blade.php
@@ -1,41 +1,31 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ログインフォーム</title>
-<script src="{{ asset('js/app.js') }}"></script>
-<link rel="stylesheet" href="{{ asset('css/app.css') }}">
-<link rel="stylesheet" href="{{ asset('css/signin.css') }}">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>ログインフォーム</title>
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  <link href="{{ asset('css/signin.css') }}" rel="stylesheet">
+  <script src="{{ asset('js/app.js') }}" defer></script>
 </head>
 <body>
-    
-<form class="form-signin" method="POST" action="{{ route('login') }}">
- @csrf 
-    <img src="{{asset('img/LOGO.png')}}">
-  <h1 class="h3 mb-3 font-weight-normal">ログインしてください</h1>
- @if   ($errors->any())
-  <div class="alert alert-danger">
-    <ul>
-        @foreach ($errors->all() as $error)
-            <li>{{$error}}</li>
-        @endforeach
+  <form class="form-signin" method="POST" action="{{ route('login') }}">
+    @csrf
+    <h1 class="h3 mb-3 font-weight-normal">ログインフォーム </h1>
+    @foreach ($errors->all() as $error)
+    <ul class="alert alert-danger">
+      <li>{{$error}}</li>
     </ul>
-   </div>
- @endif
-  <label for="inputEmail" class="sr-only">Email address</label>
-  <input name="email" type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-  <label for="inputPassword" class="sr-only">パスワード</label>
-  <input name="password" type="password" id="inputPassword" class="form-control" placeholder="Password" required>
-  <div class="checkbox mb-3">
-    <!-- <label>
-      <input type="checkbox" value="remember-me"> Remember me
-    </label> -->
-  </div>
-  <button class="btn btn-lg btn-primary btn-block" type="submit">ログイン</button>
-</form>
+    @endforeach
+    <x-alert type="danger" :session="session('logout')"/>
+    <x-alert type="danger" :session="session('danger')"/>
 
-
+    <label for="inputEmail" class="sr-only">Email address</label>
+    <input type="email" id="inputEmail" name="email" class="form-control" placeholder="Email address" required autofocus>
+    <label for="inputPassword" class="sr-only">Password</label>
+    <input type="password" id="inputPassword" name="password" class="form-control" placeholder="Password" required>
+    <button class="btn btn-lg btn-primary btn-block" type="submit">ログイン</button>
+  </form>
 </body>
 </html>
