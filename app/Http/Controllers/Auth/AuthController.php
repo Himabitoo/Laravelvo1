522555ce223c61e414043a1fbf1af91d23ff52ca
NoXeL <luxeno103@gmail.com>
2022-02-22 13:30:56 +0900

    CreateAccountLock

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginFormRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class AuthController extends Controller
 {
@@ -24,19 +25,55 @@ class AuthController extends Controller
      */
 
     public function login(LoginFormRequest $request){
+        
 
+        //ユーザー取得
         $credentials =  $request->only('email','password');
 
-        if(Auth::attempt($credentials)){
-            $request->session()->regenerate();
+        //アカウントがロックされていたら弾く
+        $user = User::where('email','=',$credentials['email'])->first();
 
-            return redirect()->route('home')->with('login_success','ログイン成功しました。');
+        if(!is_null($user)){
+
+            if($user->locked_flg === 1){
+                return back()->withErrors([
+                    'danger' => 'アカウントがロックされています。',
+                ]);;
+            }
+            if(Auth::attempt($credentials)){
+                $request->session()->regenerate();
+                    //ロック数初期設定が必要な時だけ動かす処理
+                    if($user->error_count > 0){
+                        $user->error_count = 0;
+                        //se
+                        $user->save();
+                    }
+                return redirect()->route('home')->with('login_success','ログイン成功しました。');
+            }
+
+            //⓷ログイン失敗したらエラーカウントを１増やす
+
+            $user->error_count = $user->error_count + 1;
+
+            //⓸エラーカウントが６以上の場合はアカウントをロックする
+            if($user->error_count > 5){
+
+                $user->locked_flg = 1;
+
+                return back()->withErrors([
+                    'danger' => 'アカウントがロックされました。解除されたい場合は運営に連絡してください。',
+                ]);;
+
+            }
+
+            $user->save();
+
         }
 
         return back()->withErrors([
             'login_error' => 'メールアドレスかパスワードが間違っています。',
         ]);;
-
+        
     }
 
     /**
